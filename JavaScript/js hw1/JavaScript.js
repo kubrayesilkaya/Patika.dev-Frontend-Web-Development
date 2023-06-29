fc6c00b579415958416b20940add602577632b01
Kübra Yeşilkaya <kubrayesilkaya.ky@gmail.com>
2023-06-29 21:49:20 +0300

    Patika.dev | Frontend Web Development Path | homework and studies

---
+++ JavaScript/js hw1/JavaScript.js
@@ -0,0 +1,13 @@
+﻿let userName = prompt("Lütfen Adinizi Giriniz")
+let myName = document.querySelector('#myName')
+myName.innerHTML = userName;
+
+function showTime() {
+    let myClock = document.querySelector("#myClock");
+    let date = new Date();
+
+    let days = ['Pazar', 'Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi']
+    myClock.innerHTML = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()} ${days[date.getDay()]} `
+}
+
+setInterval(showTime, 100);
